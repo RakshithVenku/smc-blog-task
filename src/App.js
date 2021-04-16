541de234d0f74c25c29415f8e66aa99eef07172d
Rakshith I <raksht7@gmail.com>
2021-04-16 19:08:21 +0530

    Added Components folder, AccordianList and Spinner

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React, {useState, useEffect} from 'react'
 import axios from 'axios'
+import Typography from '@material-ui/core/Typography'
+import Container from '@material-ui/core/Container'
+import AccordianList from './Components/AccordianList'
 
 
 const App = (props) => {
-  const [blogData, setBlogData] = useState({})
+  const [blogData, setBlogData] = useState([])
+  const [blogItems, setBlogItems] = useState([])
 
   const url = 'https://dct-cors.herokuapp.com/https://www.feedforall.com/blog-feed.xml'
 
@@ -14,19 +18,39 @@ const App = (props) => {
          .then((response) => {
            const data = response.data
            parseString(data, function (err, result) {
-            console.log(result)
-            setBlogData(result)
-           });
+           console.log(result?.rss?.channel[0]?.item)
+           setBlogData(result?.rss?.channel[0]?.item)
+
+           const result1 = result?.rss?.channel[0]?.item.filter((ele,i) => {
+               if(i < 15){
+                 return ele
+               }
+           })
+           setBlogItems(result1)
+         });
          })
          .catch((err) => {
            console.log(err.message)
          })
   },[])
 
+  const handleBtn = () => {
+    const arr = blogData.filter((blog,i) => {
+         if(i < (blogItems.length + 15)){
+           return blog
+         }
+    })
+    console.log(arr)
+
+    setBlogItems(arr)
+    
+}
+
   return (
-    <div>
-      <h1 style={{textAlign : 'center'}}>Hello world</h1>
-    </div>
+    <Container>
+      <Typography variant='h3' align='center'>Blog Data</Typography>
+      {blogData.length > 0 && <AccordianList blogData={blogData} handleBtn={handleBtn} />}
+    </Container>
   )
 }
 
